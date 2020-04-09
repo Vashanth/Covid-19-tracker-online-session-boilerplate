b48e74c408bfde28b623c12c3053e9e9d4bf9484
Vashanth <2000.vashanth@gmail.com>
2020-04-10 01:18:41 +0530

    search

--- covid.js
+++ covid.js
@@ -14,3 +14,24 @@ var dispCountry = () =>{
         })
     })
 }
+
+var searchCountry = () =>{
+    let htmlHolder = ""
+    let searchVal = document.getElementById("searcher").value
+    document.getElementById("tab").innerHTML=""
+    fetch('https://coronavirus-19-api.herokuapp.com/countries')
+    .then(data=>data.json())
+    .then(data=>{
+        data=data.filter(data=>data.country.toLowerCase()==searchVal.toLowerCase()||searchVal=="")
+        console.log(data)
+        data.map((val,index)=>{
+            htmlHolder+="<tr><th scope='row'>"+index+"</th>"
+            htmlHolder+="<td>"+val.country+"</td>"
+            htmlHolder+="<td>"+val.deaths+"</td>"
+            htmlHolder+="<td>"+val.recovered+"</td>"
+            htmlHolder+="<td>"+val.active+"</td></tr>"
+            document.getElementById("tab").innerHTML+=htmlHolder
+            htmlHolder=""
+        })
+    })
+}
